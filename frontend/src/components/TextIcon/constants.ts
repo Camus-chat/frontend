d5d9813f7151006e73b6b12c3c19a9dd63135e93
minjeong <minj_1203@naver.com>
2024-04-24 15:56:25 +0900

    feat(texticon-component): text icon 공통 컴포넌트 제작

---
+++ frontend/src/components/TextIcon/constants.ts
@@ -0,0 +1,15 @@
+export const TEXT_ICON = [
+  'private',
+  'group',
+  'strong',
+  'moderate',
+  'weak',
+] as const;
+
+export const CONTENT = {
+  private: '개인채팅',
+  group: '그룹채팅',
+  strong: '강',
+  moderate: '중',
+  weak: '약',
+};
---
+++ frontend/src/components/TextIcon/type.d.ts
@@ -0,0 +1,3 @@
+import { TEXT_ICON } from '@/components/TextIcon/constants';
+
+export type textIconType = (typeof TEXT_ICON)[number];
