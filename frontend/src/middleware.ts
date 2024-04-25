a7fdf386d6f17067777a2317a187c4675a4b9f6c
김세진 <sejin1398@gmail.com>
2024-04-25 11:04:09 +0900

    feat: subdomain 처리를 위한 middleware 추가

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server'; // This function can be marked `async` if using `await` inside
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    '/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)',
+  ],
+};
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const hostname = request.headers.get('host');
+
+  if (hostname !== 'localhost:3000') {
+    const subdomain = hostname?.split('.')[0];
+    return NextResponse.rewrite(
+      new URL(`${subdomain}${request.nextUrl.pathname}`, request.url),
+    );
+  }
+
+  return NextResponse.next();
+}
