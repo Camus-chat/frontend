9f3178b931b2d78986b789a50a9d45cecf84efc8
김세진 <sejin1398@gmail.com>
2024-04-30 17:49:41 +0900

    fix(header-component): 로그인 버튼에 onClick 이벤트 추가

--- frontend/src/components/Header/LoginMenu.tsx
+++ frontend/src/components/Header/LoginMenu.tsx
@@ -1,13 +1,19 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import styles from './index.module.scss';
 import Button from '@/components/Button';
 
 const LoginMenu = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.loginMenu}>
-      <Button size='large' color='login'>
+      <Button size='large' color='login' onClick={() => router.push('/login')}>
         로그인
       </Button>
-      <Button size='large' color='black'>
+      <Button size='large' color='black' onClick={() => router.push('/signup')}>
         회원가입
       </Button>
     </div>
