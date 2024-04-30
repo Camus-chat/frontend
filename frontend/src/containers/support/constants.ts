04f44dfcbfda3c5afa2dfef5d33bc25ca6126a3b
minjeong <minj_1203@naver.com>
2024-04-30 11:39:36 +0900

    feat(support-page): 도입 문의하기 페이지 제작

---
+++ frontend/src/containers/support/constants.ts
@@ -0,0 +1,16 @@
+export const MEMBER_OPTION: DropDownItems[] = [
+  { value: 'personal', name: '개인' },
+  { value: 'enterprise', name: '기업' },
+];
+
+export const REQUEST_OPTION: DropDownItems[] = [
+  { value: 'service', name: '서비스' },
+  { value: 'bills', name: '요금' },
+];
+
+export const ROUTE_OPTION: DropDownItems[] = [
+  { value: 'search', name: '포털 사이트 검색' },
+  { value: 'social-media', name: '소셜 미디어' },
+  { value: 'news', name: '언론 보도 기사' },
+  { value: 'etc', name: '기타' },
+];
--- frontend/src/containers/support/request/index.tsx
+++ frontend/src/containers/support/request/index.tsx
@@ -1,3 +1,5 @@
+import { MEMBER_OPTION, ROUTE_OPTION } from '@/containers/support/constants';
+
 import styles from './index.module.scss';
 import Button from '@/components/Button';
 import Dropdown from '@/components/Form/DropDown';
@@ -5,28 +7,24 @@ import Input from '@/components/Form/Input';
 import TextArea from '@/components/Form/TextArea';
 
 const Request = () => {
-  const memberOption: DropDownItems[] = [
-    { value: 'personal', name: '개인' },
-    { value: 'enterprise', name: '기업' },
-  ];
-
-  const requestOption: DropDownItems[] = [
-    { value: 'service', name: '서비스' },
-    { value: 'bills', name: '요금' },
-  ];
-
   return (
     <>
-      <p className={styles.title}> 1:1 문의하기</p>
-      <Dropdown name='회원 유형' options={memberOption} />
-      <Input name='고객명' type='text' placeholder='고객명을 입력해주세요' />
+      <p className={styles.title}> 상담 및 도입문의</p>
+      <Dropdown
+        name='회원 유형'
+        options={MEMBER_OPTION}
+        placeholder='회원 유형을 선택해 주세요'
+      />
+      <Input name='회사명' type='text' placeholder='회사명을 입력해 주세요' />
+      <Input name='이름' type='text' placeholder='이름을 입력해 주세요' />
       <Input
         name='이메일 주소'
         type='text'
-        placeholder='이메일 주소를 입력해주세요'
+        placeholder='이메일 주소를 입력해 주세요'
       />
-      <Dropdown name='문의 분류' options={requestOption} />
-      <Input name='문의 제목' type='text' placeholder='제목을 입력해주세요' />
+      <Input name='직책' type='text' placeholder='직책을 입력해 주세요' />
+      <Dropdown name='유입 경로' options={ROUTE_OPTION} placeholder='-' />
+      <Input name='문의 제목' type='text' placeholder='제목을 입력해 주세요' />
       <TextArea name='문의 내용' />
       <Button size='large' color='blue'>
         문의하기
