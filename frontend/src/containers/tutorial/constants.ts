04c6d5637ad93f43f375ec4a668decb9762ce009
전민정 <minj_1203@naver.com>
2024-05-02 00:43:12 +0900

    feat(tutorial-page): tutorial layout 생성, route 작업

---
+++ frontend/src/containers/tutorial/constants.ts
@@ -0,0 +1,22 @@
+export const FILTER_OPTION = [
+  { value: 'strong', name: '강' },
+  { value: 'moderate', name: '중' },
+  { value: 'weak', name: '약' },
+];
+
+export const FILTER_DESCRIPTION = {
+  title: '필터링 강도에 대한 어쩌고 저쩌고',
+  content: '필터링 강도를 설정하여 안전한 대화를 어쩌고 저쩌고',
+};
+
+export const CHANNEL_DESCRIPTION = [
+  {
+    title: '채팅 유형을 선택하세요',
+    content: '프라이빗한 1:1 채팅을 원하시나요?',
+  },
+];
+
+export const CHANNEL_CREATE = {
+  title: '채널 생성',
+  content: '채팅 채널 생성에 필요한 정보를 입력해주세요.',
+};
