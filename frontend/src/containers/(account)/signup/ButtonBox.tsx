c03321de7112b63291bf91a0b7ef3846cf2b0733
minjeong <minj_1203@naver.com>
2024-05-02 14:08:41 +0900

    feat(signup-page): ButtonBox - hover Style Props로 받는 기능/로직 추가

--- frontend/src/containers/(account)/signup/ButtonBox.tsx
+++ frontend/src/containers/(account)/signup/ButtonBox.tsx
@@ -5,22 +5,34 @@ import styles from '@/containers/(account)/signup/index.module.scss';
 import Button from '@/components/Button';
 
 interface Props {
-  prev: string;
+  prev?: string;
   clickPrev?: () => void;
   clickNext: () => void;
   nextColor: string;
+  nextHover: string;
 }
 
-const ButtonBox = ({ prev, clickPrev, clickNext, nextColor }: Props) => {
+const ButtonBox = ({
+  prev,
+  clickPrev,
+  clickNext,
+  nextColor,
+  nextHover,
+}: Props) => {
   return (
     <div className={styles.buttonWrapper}>
-      <div className={classNames(styles.prevButton, styles[prev])}>
+      <div className={classNames(styles.prevButton, { [styles.prev]: prev })}>
         <Button size='large' color='login' onClick={clickPrev}>
           이전
         </Button>
       </div>
       <div className={styles.nextButton}>
-        <Button size='large' color={nextColor} onClick={clickNext}>
+        <Button
+          size='large'
+          color={nextColor}
+          onClick={clickNext}
+          hover={nextHover}
+        >
           다음
         </Button>
       </div>
