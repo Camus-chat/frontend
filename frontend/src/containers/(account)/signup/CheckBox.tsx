a1c51c3010c30199c09604fe9fd16d0a0eac30ae
minjeong <minj_1203@naver.com>
2024-05-02 13:38:37 +0900

    feat(signup-page): 약관 동의 컴포넌트 제작

--- frontend/src/containers/(account)/signup/CheckBox.tsx
+++ frontend/src/containers/(account)/signup/CheckBox.tsx
@@ -1,18 +1,27 @@
 import classNames from 'classnames';
+import React, { ChangeEvent } from 'react';
 
 import styles from './index.module.scss';
 
 interface Props {
   content: string;
   color: string;
+  isChecked: boolean;
   isEssential?: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const CheckBox = ({ content, color, isEssential }: Props) => {
+const CheckBox = ({
+  content,
+  color,
+  isChecked,
+  isEssential,
+  onChange,
+}: Props) => {
   return (
     <div className={classNames(styles.checkBox, styles[color])}>
       <label>
-        <input type='checkbox' />
+        <input type='checkbox' checked={isChecked} onChange={onChange} />
         {isEssential && <span>[필수]</span>}
         {content}
       </label>
