7fc90244d2f92c6e7d1e7cdc44cbfd8df0a2a547
전민정 <minj_1203@naver.com>
2024-05-06 21:40:50 +0900

    feat(tutorial-page): 채널 튜토리얼 페이지 제작

--- frontend/src/containers/tutorial/channel/index.tsx
+++ frontend/src/containers/tutorial/channel/index.tsx
@@ -1,29 +1,36 @@
-import SelectButton from '@/containers/(account)/login/SelectButton';
+import ChannelInfo from '@/containers/tutorial/channel/ChannelInfo';
+import CreateButton from '@/containers/tutorial/channel/CreateButton';
+import styles from '@/containers/tutorial/channel/index.module.scss';
+import SelectChatType from '@/containers/tutorial/channel/SelectChatType';
 import {
   CHANNEL_CREATE,
   CHANNEL_DESCRIPTION,
+  FILTER_OPTION,
 } from '@/containers/tutorial/constants';
-import styles from '@/containers/tutorial/index.module.scss';
 
+import DropDown from '@/components/Form/DropDown';
 import InfoTextBox from '@/components/InfoTextBox';
 
 const Channel = () => {
-  /* TODO
-      - 애니메이션 : keyframe 사용해서 애니메이션 추가하기
-      - 페이드 인 에니메이션 추가됨에 따라 우측의 description 문구 변경 (constants로 관리하기)
-      - selectButton 공컴으로 빼기
-
-    */
   return (
     <div className={styles.tutorialWrapper}>
       <div className={styles.blueBox}>
-        <div className={styles.contentBox}>
+        <div className={styles.createChannelBox}>
           <InfoTextBox
             size='small'
             title={CHANNEL_CREATE.title}
             content={CHANNEL_CREATE.content}
           />
-          <SelectButton />
+          <div>
+            <SelectChatType />
+          </div>
+          <div>
+            <ChannelInfo />
+          </div>
+          <div>
+            <DropDown name='필터링 강도' options={FILTER_OPTION} />
+            <CreateButton />
+          </div>
         </div>
       </div>
       <div className={styles.textBox}>
--- frontend/src/containers/tutorial/constants.ts
+++ frontend/src/containers/tutorial/constants.ts
@@ -12,7 +12,15 @@ export const FILTER_DESCRIPTION = {
 export const CHANNEL_DESCRIPTION = [
   {
     title: '채팅 유형을 선택하세요',
-    content: '프라이빗한 1:1 채팅을 원하시나요?',
+    content: '프라이빗한 1:1 채팅을 원하시나요 블라블라',
+  },
+  {
+    title: '채팅 정보를 입력하세요',
+    content: '채널명과 소개글을 입력하여 어쩌고 저쩌고',
+  },
+  {
+    title: '필터링 강도를 설정하세요',
+    content: '필터링 강도를 설정하고 생성하기 버튼을 눌러주세요',
   },
 ];
 
@@ -20,3 +28,6 @@ export const CHANNEL_CREATE = {
   title: '채널 생성',
   content: '채팅 채널 생성에 필요한 정보를 입력해주세요.',
 };
+
+export const PRIVATE = true;
+export const GROUP = false;
