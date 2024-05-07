a5211925326b701d67201abc861d63ef2a2fe3c6
minjeong <minj_1203@naver.com>
2024-05-07 16:14:00 +0900

    feat(api): chat type 선언

--- frontend/src/types/channel.d.ts
+++ frontend/src/types/channel.d.ts
@@ -6,11 +6,3 @@ interface ChannelInfo {
   filter: string;
   isAvailable: boolean;
 }
-
-interface ChatRoom {
-  id: string;
-  channelName: string;
-  createTime: string;
-  userList: string[]; // TODO: user class 로 받음. 타입 수정 해야함.
-  isClosed: boolean;
-}
