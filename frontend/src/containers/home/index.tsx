9d3e5345c8e3431c3e08a5fae33a4e47b26fc80a
minjeong <minj_1203@naver.com>
2024-05-08 17:08:53 +0900

    feat(landing-pae): 랜딩 페이지 제작

--- frontend/src/containers/home/index.tsx
+++ frontend/src/containers/home/index.tsx
@@ -1,7 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { DESCRIPTION } from '@/containers/home/constants';
+import styles from '@/containers/home/index.module.scss';
+import InfoCard from '@/containers/home/InfoCard';
+
 import Header from '@/components/Header';
 
 const Home = () => {
-  return <Header />;
+  // TODO: 이미지 바꿔야함.
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.firstWrapper}>
+          <div className={styles.content}>
+            <pre> {DESCRIPTION} </pre>
+            <Link href='/tutorial/channel'>자세히 알아보기</Link>
+          </div>
+          <div className={styles.image}>
+            <Image
+              src='/images/landing.svg'
+              alt='image'
+              width='422'
+              height='526'
+            />
+          </div>
+        </div>
+        <div className={styles.secondWrapper}>
+          <InfoCard />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
