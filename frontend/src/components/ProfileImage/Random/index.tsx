0736c9161feb2c7a23aae31998e20c3d8cb74b23
minjeong <minj_1203@naver.com>
2024-05-08 10:28:15 +0900

    fix(profileimage-component): 랜덤 이미지 컬러 type으로 선언

--- frontend/src/components/ProfileImage/Random/index.tsx
+++ frontend/src/components/ProfileImage/Random/index.tsx
@@ -3,12 +3,15 @@ import PersonIcon from '@mui/icons-material/Person';
 import classNames from 'classnames';
 
 import styles from '@/components/ProfileImage/index.module.scss';
-import { profileImageType } from '@/components/ProfileImage/type';
+import {
+  profileColorType,
+  profileImageType,
+} from '@/components/ProfileImage/type';
 
 interface Props {
   group?: boolean;
   size: profileImageType;
-  color: string;
+  color: profileColorType;
 }
 
 const Random = ({ group, size, color }: Props) => {
--- frontend/src/components/ProfileImage/type.d.ts
+++ frontend/src/components/ProfileImage/type.d.ts
@@ -1,3 +1,5 @@
-import { PROFILE_SIZE } from '@/components/ProfileImage/constants';
+import { COLORS, PROFILE_SIZE } from '@/components/ProfileImage/constants';
 
 export type profileImageType = (typeof PROFILE_SIZE)[number];
+
+export type profileColorType = (typeof COLORS)[number];
