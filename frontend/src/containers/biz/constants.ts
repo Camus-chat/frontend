0012f523572f4b1b51847f64c09f874c01319e62
minjeong <minj_1203@naver.com>
2024-05-08 15:11:45 +0900

    feat(landing-pae): 비즈니스 랜딩 페이지 제작

---
+++ frontend/src/containers/biz/constants.ts
@@ -0,0 +1,26 @@
+export const LANDING_DESCRIPTION = {
+  first: '안전한 채팅 서비스를 \n' + '커스텀하여 제공하세요',
+  second: '안전한 커뮤니티를 구축하고 비즈니스 경쟁력을 높이세요',
+  third: '영업 멘트 써주세용',
+};
+
+export const CARD = [
+  {
+    key: 'filter',
+    title: 'AI 세이프티 필터링',
+    path: '/docs',
+    color: 'navy',
+  },
+  {
+    key: 'introduce',
+    title: '간편한 도입 방법',
+    path: '/docs',
+    color: 'blue',
+  },
+  {
+    key: 'charge',
+    title: '최적화된 비용',
+    path: '/charge',
+    color: 'green',
+  },
+];
--- frontend/src/containers/biz/index.tsx
+++ frontend/src/containers/biz/index.tsx
@@ -1,7 +1,44 @@
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+
+import Cards from '@/containers/biz/Cards';
+import { LANDING_DESCRIPTION } from '@/containers/biz/constants';
+import styles from '@/containers/biz/index.module.scss';
+
 import Header from '@/components/Header';
 
 const BizHome = () => {
-  return <Header biz />;
+  return (
+    <>
+      <Header biz />
+      <div className={styles.container}>
+        <div className={styles.firstWrapper}>
+          <div className={styles.content}>
+            <pre className={styles.description}>
+              {LANDING_DESCRIPTION.first}
+            </pre>
+            <button className={styles.moreButton}>자세히 알아보기</button>
+          </div>
+          <div className={styles.imageBox}>이미지가 들어가야 합니다.</div>
+        </div>
+        <div className={styles.secondWrapper}>
+          <div className={styles.content}>
+            <div className={styles.description}>
+              {LANDING_DESCRIPTION.second}
+            </div>
+            <button className={styles.moreButton}>서비스 전체보기</button>
+          </div>
+          <Cards />
+        </div>
+        <div className={styles.thirdWrapper}>
+          <div className={styles.description}>{LANDING_DESCRIPTION.third}</div>
+          <button>
+            <p>문의하기</p>
+            <ExpandCircleDownIcon />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BizHome;
