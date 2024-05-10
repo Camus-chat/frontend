b324eb5aa3a9b7ffd78c327281316924771e6567
minjeong <minj_1203@naver.com>
2024-05-10 13:47:16 +0900

    feat(login-api): login api 요청 함수 작성

---
+++ frontend/src/containers/(account)/login/query.ts
@@ -0,0 +1,14 @@
+import { api } from '@/containers/query';
+
+export const postLogin = async (isEnterprise: boolean, account: LogIn) => {
+  try {
+    if (!isEnterprise) {
+      const response = await api.post(false, '/api/member/b2c/login', account);
+      return response;
+    }
+    const response = await api.post(false, '/api/member/b2b/login', account);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
