fcc228574f6a5e67489eab69b5cae812fc82897c
minjeong <minj_1203@naver.com>
2024-05-10 13:46:58 +0900

    feat(channel-api): channel 생성 api 요청 함수 작성

---
+++ frontend/src/containers/service/query.ts
@@ -0,0 +1,30 @@
+import { api } from '@/containers/query';
+
+interface RoomInfo {
+  type: string;
+  title: string;
+  content: string;
+  filterLevel: number;
+}
+
+interface channel {
+  type: string;
+  title: string;
+  content: string;
+  filterLevel: number;
+  link: string;
+}
+
+export const postCreateChannel = async (channelInfo: RoomInfo) => {
+  try {
+    const response = await api.post<channel, RoomInfo>(
+      false,
+      'channel/create',
+      channelInfo,
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
