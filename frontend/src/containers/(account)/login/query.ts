00ebddadc38184e1a0609fb4eaa782cb0ab3c465
minjeong <minj_1203@naver.com>
2024-05-10 15:01:34 +0900

    refactor(login-query): login api 요청 함수 수정

--- frontend/src/containers/(account)/login/query.ts
+++ frontend/src/containers/(account)/login/query.ts
@@ -1,18 +1,14 @@
 import { query } from '@/containers/query';
 
-export const postLogin = async (isEnterprise: boolean, account: Account) => {
-  try {
-    if (!isEnterprise) {
-      const response = await query.post(
-        false,
-        '/api/member/b2c/login',
-        account,
-      );
-      return response;
-    }
-    const response = await query.post(false, '/api/member/b2b/login', account);
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+export const Login = async (isEnterprise: boolean, account: Account) => {
+  const apiUrl = isEnterprise ? '/member/b2b/login' : '/member/b2c/login';
+  return query
+    .post(false, apiUrl, account)
+    .then((res) => {
+      console.log(res);
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
