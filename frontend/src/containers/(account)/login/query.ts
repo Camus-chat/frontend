231743ac9d81c31435bbd24ad5f1d86aadebd4b9
전민정 <minj_1203@naver.com>
2024-05-12 19:32:08 +0900

    feat(login-api): 로그인 api 제작

--- frontend/src/containers/(account)/login/query.ts
+++ frontend/src/containers/(account)/login/query.ts
@@ -1,19 +1,21 @@
 import { query } from '@/containers/query';
 
+interface ResponseToken {
+  accessToken: string;
+  refreshToken: string;
+}
+
 export const requestLogin = async (isEnterprise: boolean, account: Account) => {
   const apiUrl = isEnterprise ? '/member/b2b/login' : '/member/b2c/login';
   return query
-    .post(false, apiUrl, account)
+    .post<ResponseToken, Account>(false, apiUrl, account)
     .then((res) => {
-      if (res.status === 200) {
-        // TODO: refresh-token 에 대한 이야기
-        const acessToken = res.data.accessToken;
-        localStorage.setItem(acessToken, acessToken);
-        return true;
-      }
-      return false;
+      // TODO: refresh-token 에 대한 이야기 나눠볼 것. 김세진과
+      localStorage.setItem('accessToken', res.accessToken);
+      return true;
     })
     .catch((err) => {
       console.log(err);
+      return false;
     });
 };
