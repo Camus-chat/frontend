bf658b28d5cdf7a298debde530d53571cadef16c
minjeong <minj_1203@naver.com>
2024-05-14 16:59:09 +0900

    fix(login-api): api 로직 수정 후 pull, 변수명 수정

--- frontend/src/containers/(account)/login/query.ts
+++ frontend/src/containers/(account)/login/query.ts
@@ -7,8 +7,8 @@ interface ResponseToken {
 
 export const requestLogin = async (isEnterprise: boolean, account: Account) => {
   const apiUrl = isEnterprise ? '/member/b2b/login' : '/member/b2c/login';
-  return query
-    .post<ResponseToken, Account>(false, apiUrl, account)
+  return query.clientSide
+    .post<ResponseToken, Account>(apiUrl, account)
     .then((res) => {
       // TODO: refresh-token 에 대한 이야기 나눠볼 것. 김세진과
       localStorage.setItem('accessToken', res.accessToken);
