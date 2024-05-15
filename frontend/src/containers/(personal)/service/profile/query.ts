7bafa8a9fe5359498cdf835d90c0a649af28d95b
전민정 <minj_1203@naver.com>
2024-05-15 18:36:52 +0900

    feat(profile-api): 프로필 api 요청 함수 작성

--- frontend/src/containers/(personal)/service/profile/query.ts
+++ frontend/src/containers/(personal)/service/profile/query.ts
@@ -14,3 +14,21 @@ export const requestProfile = () => {
       return defaultProfile;
     });
 };
+
+export const updateProfileImg = (image: File) => {
+  return query.clientSide
+    .patch<boolean, File>('api/member/b2c/image', image)
+    .then((res) => {
+      return !!res;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const updateNickname = (nickname: string) => {
+  return query.clientSide
+    .patch<boolean, string>('api/member/b2c/nickname', nickname)
+    .then((res) => {
+      return !!res;
+    })
+    .catch((err) => console.log(err));
+};
