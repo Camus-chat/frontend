5f94e343957fc3078f33395823ca5417a9ff2f20
전민정 <minj_1203@naver.com>
2024-05-15 21:40:48 +0900

    feat(profile-api): 프로필 수정 api 요청 함수 작성

--- frontend/src/containers/(personal)/service/profile/query.ts
+++ frontend/src/containers/(personal)/service/profile/query.ts
@@ -1,34 +1,21 @@
-import type { Profiles } from '@/containers/(personal)/service/profile/type';
 import { query } from '@/containers/query';
 
-export const requestProfile = () => {
-  const defaultProfile: Profiles = { profileImg: '', nickname: '' };
-
-  return query.serverSide
-    .get<Profiles>('/api/member/b2c/info')
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log(err);
-      return defaultProfile;
-    });
-};
-
-export const updateProfileImg = (image: File) => {
+export const updateProfileImg = async (image: File) => {
   return query.clientSide
-    .patch<boolean, File>('api/member/b2c/image', image)
+    .patch<string, File>('/member/b2c/image', image)
     .then((res) => {
-      return !!res;
+      console.log(res);
+      return res;
     })
     .catch((err) => console.log(err));
 };
 
-export const updateNickname = (nickname: string) => {
+export const updateNickname = async (nickname: string) => {
   return query.clientSide
-    .patch<boolean, string>('api/member/b2c/nickname', nickname)
+    .patch<string, string>('/member/b2c/nickname', nickname)
     .then((res) => {
-      return !!res;
+      console.log(res);
+      return res;
     })
     .catch((err) => console.log(err));
 };
