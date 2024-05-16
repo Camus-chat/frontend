dc060e54ee576347abb56923e8d99355deb4496b
김세진 <sejin1398@gmail.com>
2024-05-16 13:03:22 +0900

    feat(channelcreate-query): 채널 생성 기능 추가

--- frontend/src/containers/(personal)/service/channel/query.ts
+++ frontend/src/containers/(personal)/service/channel/query.ts
@@ -1,6 +1,18 @@
-import type { Channel } from '@/containers/(personal)/service/channel/type';
+import type {
+  Channel,
+  RequestChannelCreate,
+} from '@/containers/(personal)/service/channel/type';
 import { query } from '@/containers/query';
 
 export const getChannels = async () => {
   return query.serverSide.get<Channel[]>('/channel/list');
+};
+
+export const createChannel = async (request: RequestChannelCreate) => {
+  await query.clientSide
+    .post<Channel, RequestChannelCreate>('/channel/create', request)
+    .then((res) => {
+      console.log(res);
+      return res;
+    });
 };
