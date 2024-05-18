b8333eaf574378fefaaf5c901b5ba39944ae5cc9
minjeong <minj_1203@naver.com>
2024-05-18 16:59:55 +0900

    feat(header-component): 2중 헤더 컴포넌트 제작 - 튜토리얼 페이지 헤더 삽입

--- frontend/src/containers/(personal)/tutorial/index.tsx
+++ frontend/src/containers/(personal)/tutorial/index.tsx
@@ -1,7 +1,10 @@
 import { ReactNode } from 'react';
 
+import { MENU } from '@/containers/(personal)/tutorial/constants';
+
 import styles from './index.module.scss';
 import Header from '@/components/Header';
+import ListMenu from '@/components/Header/ListMenu';
 
 interface Props {
   children: ReactNode;
@@ -11,6 +14,7 @@ const Tutorial = ({ children }: Props) => {
   return (
     <>
       <Header />
+      <ListMenu menu={MENU} />
       <div className={styles.container}>{children}</div>
     </>
   );
