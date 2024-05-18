4255eb472e33e148563db151dfc815407f3a6cd1
김세진 <sejin1398@gmail.com>
2024-05-19 05:49:10 +0900

    chore(router): 게스트 라우팅 주소 변경

--- frontend/src/containers/guest/[link]/index.tsx
+++ frontend/src/containers/guest/[link]/index.tsx
@@ -1,11 +1,8 @@
 // import { GetServerSideProps } from 'next';
 
-import { Entry } from '@/containers/(guest)/[link]/Entry';
-import styles from '@/containers/(guest)/[link]/index.module.scss';
-import {
-  requestChannelInfo,
-  requestGuestProfile,
-} from '@/containers/(guest)/[link]/query';
+import {Entry} from '@/containers/guest/[link]/Entry';
+import styles from '@/containers/guest/[link]/index.module.scss';
+import {requestChannelInfo, requestGuestProfile,} from '@/containers/guest/[link]/query';
 
 import Logo from '@/components/Header/Logo';
 
