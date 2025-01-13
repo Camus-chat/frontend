c1dcb37d95bf5a9140d3f904ec5e154fd727b57c
nijesmik <bkybb@duck.com>
2025-01-13 17:37:23 +0900

    :ambulance: 충돌 발생하는 부분 임시 주석 처리

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
 
 import { NAVIGATIONS } from './constants';
-import { query } from '@/containers/query';
 
 import styles from './index.module.scss';
-import Drawer from '@/components/Header/Drawer';
-import LoginMemberInfo from '@/components/Header/LoginMemberInfo';
+// import Drawer from '@/components/Header/Drawer';
+// import LoginMemberInfo from '@/components/Header/LoginMemberInfo';
 import LoginMenu from '@/components/Header/LoginMenu';
 import Logo from '@/components/Header/Logo';
 import NavMenu from '@/components/Header/NavMenu';
@@ -15,8 +14,8 @@ interface Props {
 }
 
 const Header = async ({ biz }: Props) => {
-  const memberInfo = await query.serverSide.get<MemberInfo>('/member/b2c/info');
-  const signedIn = !!memberInfo.nickname;
+  // const memberInfo = await query.serverSide.get<MemberInfo>('/member/b2c/info');
+  const signedIn = false;
 
   const navItems = biz ? NAVIGATIONS.business : NAVIGATIONS.personal;
 
@@ -33,13 +32,13 @@ const Header = async ({ biz }: Props) => {
         <NavMenu navItems={navItems} />
         <div className={styles.loginMenu}>
           {!signedIn && <LoginMenu />}
-          <LoginMemberInfo signedIn={signedIn} info={memberInfo} />
+          {/*<LoginMemberInfo signedIn={signedIn} info={memberInfo} />*/}
         </div>
-        <Drawer
-          navItems={navItems}
-          signedIn={signedIn}
-          loginMemberInfo={memberInfo}
-        />
+        {/*<Drawer*/}
+        {/*  navItems={navItems}*/}
+        {/*  signedIn={signedIn}*/}
+        {/*  loginMemberInfo={memberInfo}*/}
+        {/*/>*/}
       </Logo>
     </header>
   );
