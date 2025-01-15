0ef1eb8444cb56604664c0eec4a4e7d7ec696d50
nijesmik <bkybb@duck.com>
2025-01-15 17:32:15 +0900

    :lipstick: navigation bar 스타일 수정 #4

--- src/features/nav-bar/ui/index.tsx
+++ src/features/nav-bar/ui/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const NavigationBar = ({ children }: Props) => {
   return (
-    <div className='flex justify-between max-w-screen-xl mx-auto py-4 sm:py-3 px-5 md:px-7 xl:px-5'>
+    <div className='flex items-center justify-between max-w-screen-xl mx-auto py-4 md:py-3 px-5 md:px-7 xl:px-5'>
       <Logo />
       {children}
     </div>
