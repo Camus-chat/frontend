ea15034f8ba2340c179128828926e34aa4fe2450
nijesmik <bkybb@duck.com>
2025-01-16 16:19:40 +0900

    :fire: 서브도메인 확인 api 삭제 #4

--- app/api/host/route.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest } from 'next/server';
-
-export async function GET(request: NextRequest) {
-  const host = request.headers.get('host');
-
-  return Response.json(host);
-}
--- src/features/nav-bar/api/getIsBizDomain.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { BASE_URL } from '@/shared/config';
-
-export const getIsBizDomain = async () => {
-  const host = await fetch(`${BASE_URL}/api/host`).then((res) => res.json());
-  return host.startsWith('biz');
-};
