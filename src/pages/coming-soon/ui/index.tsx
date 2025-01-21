4abef85df359b82be2156ee682f4b3072432aa0d
nijesmik <bkybb@duck.com>
2025-01-22 02:22:07 +0900

    :art: prettier 적용

--- src/pages/coming-soon/ui/index.tsx
+++ src/pages/coming-soon/ui/index.tsx
@@ -1,20 +1,27 @@
 import Image from 'next/image';
 
-export default function ComingSoon(){
+export default function ComingSoon() {
   return (
-    <div className="m-0 w-full h-full bg-white flex justify-around">
-      <div className="bg-transparent flex items-center [&>*]:bg-transparent ml-52">
-        <Image src="/images/comingsoon.svg" alt="이미지" width={480} height={720} />
+    <div className='m-0 flex h-full w-full justify-around bg-white'>
+      <div className='ml-52 flex items-center bg-transparent [&>*]:bg-transparent'>
+        <Image
+          src='/images/comingsoon.svg'
+          alt='이미지'
+          width={480}
+          height={720}
+        />
       </div>
-      <div className=" bg-transparent flex flex-col justify-center pb-20 mr-52">
-        <div className="bg-transparent my-8 flex flex-row justify-center items-center">
-          <Image src="/images/logo.svg" alt="이미지" width={64} height={64} />
-          <p className="text-5xl text-center text-black font-[600] ml-4">COMING SOON</p>
+      <div className='mr-52 flex flex-col justify-center bg-transparent pb-20'>
+        <div className='my-8 flex flex-row items-center justify-center bg-transparent'>
+          <Image src='/images/logo.svg' alt='이미지' width={64} height={64} />
+          <p className='ml-4 text-center text-5xl font-[600] text-black'>
+            COMING SOON
+          </p>
         </div>
-        <div className="bg-transparent text-2xl text-center text-black font-[400] my-4">
+        <div className='my-4 bg-transparent text-center text-2xl font-[400] text-black'>
           AI 안전 채팅을 통해 당신을 지켜드립니다.
         </div>
       </div>
     </div>
-  )
+  );
 }
