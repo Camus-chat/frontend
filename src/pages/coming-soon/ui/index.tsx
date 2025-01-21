12815611a492406360b9766675e438fd5fb0a90f
nijesmik <bkybb@duck.com>
2025-01-22 02:44:27 +0900

    :lipstick: coming soon 페이지 grid 적용

--- src/pages/coming-soon/ui/index.tsx
+++ src/pages/coming-soon/ui/index.tsx
@@ -1,27 +1,27 @@
 import Image from 'next/image';
 
-export default function ComingSoon() {
+const ComingSoon = () => {
   return (
-    <div className='m-0 flex h-full w-full justify-around bg-white'>
-      <div className='ml-52 flex items-center bg-transparent [&>*]:bg-transparent'>
-        <Image
-          src='/images/comingsoon.svg'
-          alt='이미지'
-          width={480}
-          height={720}
-        />
-      </div>
-      <div className='mr-52 flex flex-col justify-center bg-transparent pb-20'>
-        <div className='my-8 flex flex-row items-center justify-center bg-transparent'>
+    <div className='m-0 grid h-full w-full grid-cols-2 justify-around gap-5 bg-white'>
+      <Image
+        src='/images/comingsoon.svg'
+        alt='이미지'
+        width={480}
+        height={720}
+      />
+      <section className='flex flex-col justify-center gap-4 bg-transparent pb-20'>
+        <div className='flex flex-row items-center justify-center bg-transparent'>
           <Image src='/images/logo.svg' alt='이미지' width={64} height={64} />
-          <p className='ml-4 text-center text-5xl font-[600] text-black'>
+          <h1 className='ml-4 text-center text-5xl font-semibold text-black'>
             COMING SOON
-          </p>
+          </h1>
         </div>
-        <div className='my-4 bg-transparent text-center text-2xl font-[400] text-black'>
+        <p className='bg-transparent text-center text-xl text-black'>
           AI 안전 채팅을 통해 당신을 지켜드립니다.
-        </div>
-      </div>
+        </p>
+      </section>
     </div>
   );
-}
+};
+
+export default ComingSoon;
