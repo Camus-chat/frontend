2d0d1aecbb2e449538231d8f0ad43f9f1fc520ae
nijesmik <bkybb@duck.com>
2025-01-25 01:47:23 +0900

    :recycle: 인증 페이지 레이아웃 tailwind-variants 적용 #17

---
+++ src/app/layouts/auth/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+
+import Logo from '@/features/logo-bar';
+
+import { auth } from './styles';
+
+interface Props {
+  children: ReactNode;
+}
+
+const AccountLayout = async ({ children }: Props) => {
+  const { wrapper, header, main } = auth();
+
+  return (
+    <div className={wrapper()}>
+      <header className={header()}>
+        <Logo business={false} />
+      </header>
+      <main className={main()}>{children}</main>
+    </div>
+  );
+};
+
+export default AccountLayout;
