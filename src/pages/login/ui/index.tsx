15f7b64a7f6943fad662cf636969b845cfe6d1a6
전민정 <minj_1203@naver.com>
2025-01-24 19:11:18 +0900

    :recycle: tailwind CSS 적용 #16

--- src/pages/login/ui/index.tsx
+++ src/pages/login/ui/index.tsx
@@ -6,7 +6,6 @@ import { useRef, useState } from 'react';
 
 import { requestLogin } from '@/pages/login/api/query';
 
-import styles from '../../../containers/(account)/login/index.module.scss';
 import Button from '@/shared/ui/button';
 import Input from '@/components/Form/Input';
 
@@ -40,23 +39,25 @@ const Login = () => {
 
   return (
     <>
-      <h1 className={styles.title}> 로그인</h1>
-      <div className={styles.inputWrapper}>
+      <h1 className='text-[28px] font-bold mb-4'> 로그인</h1>
+      <div>
         <Input type='text' placeholder='아이디를 입력해주세요' ref={idRef} />
         <Input
           type='password'
           placeholder='비밀번호를 입력해주세요'
           ref={passwordRef}
         />
-        <div className={styles.buttonWrapper}>
+        <div className='py-2 flex flex-col'>
           <Button size='large' color='blue' onClick={handleClick}>
             로그인
           </Button>
-          <Link className={styles.link} href='/signup'>
-            회원가입
-          </Link>
+          <div className='py-2 flex justify-end'>
+            <Link className='text-[14px] text-gray-400 hover:text-blue-600' href='/signup'>
+              회원가입
+            </Link>
+          </div>
         </div>
-        {!isValid && <p>로그인 정보를 모두 입력해주세요.</p>}
+        {!isValid && <p className='text-red-500'>로그인 정보를 모두 입력해주세요.</p>}
       </div>
     </>
   );
