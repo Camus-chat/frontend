4ac1acabe4cd70d9bae2e7d02f73828245ab0d4a
nijesmik <bkybb@duck.com>
2025-01-24 13:12:13 +0900

    :lipstick: 인증 페이지 레이아웃 tailwind 적용 #17

--- src/containers/(account)/index.tsx
+++ src/containers/(account)/index.tsx
@@ -2,22 +2,20 @@ import { ReactNode } from 'react';
 
 import Logo from '@/features/logo-bar';
 
-import styles from './index.module.scss';
-
 interface Props {
   children: ReactNode;
 }
 
 const AccountLayout = async ({ children }: Props) => {
   return (
-    <>
-      <header className='max-md:px-2 xl:pt-8 md:py-1 xl:drop-shadow-sm'>
+    <div className='grid size-full grid-rows-[auto_1fr] md:bg-gray-100'>
+      <header className='bg-white max-md:px-2 md:py-1 xl:pt-8 xl:drop-shadow-sm'>
         <Logo business={false} />
       </header>
-      <main className={styles.main}>
-        <div className={styles.container}>{children}</div>
+      <main className='w-full rounded-2xl bg-white px-12 py-16 max-md:p-8 md:w-[610px] md:place-self-center md:shadow-[0_1px_23px_0_rgba(0,0,0,0.05)]'>
+        {children}
       </main>
-    </>
+    </div>
   );
 };
 
