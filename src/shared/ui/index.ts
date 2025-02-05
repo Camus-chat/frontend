cd256b3f799bde5b654b06fce9206482ca90ed82
nijesmik <bkybb@duck.com>
2025-02-05 18:34:08 +0900

    :truck: 파일명 변경 #18

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './button';
 export { default as Password } from './input/password';
-export { default as Input } from './input/index';
+export { default as Input } from './input/textfield';
