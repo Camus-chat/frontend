3758a62bcab4df183565d7b478bd304a53e0c42d
nijesmik <bkybb@duck.com>
2025-02-18 00:31:15 +0900

    :sparkles: server side api 요청 시 token 주입 #32

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,9 +1,9 @@
 import { auth } from './auth';
 import { client } from './client';
-import { server } from './server';
+import { get } from './server';
 
 export const callAPI = {
   auth: auth.post,
   clientSide: client,
-  serverSide: server,
+  serverSide: { get },
 };
