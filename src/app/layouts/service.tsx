995296849b10540324775ecc58db45b9d4b24826
nijesmik <bkybb@duck.com>
2025-03-02 00:57:45 +0900

    :recycle: service layout 리팩토링 #49

---
+++ src/app/layouts/service.tsx
@@ -0,0 +1,22 @@
+import type { FC, ReactNode } from 'react';
+
+import Menu from '@/containers/(personal)/service/Menu';
+import Header from '@/widgets/header';
+
+interface Props {
+  children: ReactNode;
+}
+
+const ServiceLayout: FC<Props> = ({ children }) => {
+  return (
+    <div className='grid size-full grid-rows-[auto,1fr]'>
+      <Header LogedInOnly />
+      <main className='relative md:bg-default-100'>
+        <Menu />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default ServiceLayout;
