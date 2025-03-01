6abe07226fbc5a65ad30f9b3823bd0917ab37343
nijesmik <bkybb@duck.com>
2025-03-02 01:59:08 +0900

    :sparkles: icon 추가

---
+++ src/shared/ui/icon/messages.tsx
@@ -0,0 +1,18 @@
+export const MessagesIcon = ({ size = 24 }: IconSvgProps) => (
+  <svg
+    aria-hidden='true'
+    xmlns='http://www.w3.org/2000/svg'
+    width={size}
+    height={size}
+    fill='none'
+    viewBox='0 0 24 24'
+  >
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      d='M9 17h6l3 3v-3h2V9h-2M4 4h11v8H9l-3 3v-3H4V4Z'
+    />
+  </svg>
+);
