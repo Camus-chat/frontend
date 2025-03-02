989efe04ab50286ec13266e2cb2c7f0bee22e65b
전민정 <minj_1203@naver.com>
2025-03-03 02:43:01 +0900

    :lipstick: channel-list-item tailwind 적용

--- src/containers/(personal)/service/channel/ChannelListItem/index.tsx
+++ src/containers/(personal)/service/channel/ChannelListItem/index.tsx
@@ -5,22 +5,22 @@ import { FILTER_CLASS_MAP } from '@/containers/(personal)/service/channel/consta
 import type { Channel } from '@/containers/(personal)/service/channel/type';
 import { Button } from '@/shared/ui';
 
-import styles from './index.module.scss';
-
 interface Props {
   channel: Channel;
 }
 
 const ChannelListItem = ({ channel }: Props) => {
   return (
-    <li className={styles.channelListItem}>
-      <div className={styles.info}>
-        <div className={styles.title}>{channel.title}</div>
+    <li className='hover:shadow-[0 1px 4px 0 rgba(0, 0, 0, 0.07)] m-2 flex min-h-[110px] flex-col rounded-lg border border-gray-100 p-4 hover:transition-all hover:duration-300'>
+      <div className='flex items-center'>
+        <div className='mr-0.5 overflow-hidden text-ellipsis text-xl font-semibold'>
+          {channel.title}
+        </div>
         <TextIcon name={channel.type} />
         <TextIcon name={FILTER_CLASS_MAP[channel.filterLevel]} />
       </div>
-      <div className={styles.description}>{channel.content}</div>
-      <div className={styles.buttons}>
+      <div className='py-1'>{channel.content}</div>
+      <div className='flex justify-start'>
         <Button size='sm' variant='flat' className='hover:text-red-500'>
           삭제
         </Button>
