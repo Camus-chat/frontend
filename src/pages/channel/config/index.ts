869d8e7b50eb2fedf12f62f7041c0a1e342cce9f
nijesmik <bkybb@duck.com>
2025-03-12 00:59:08 +0900

    :recycle: 채널 생성 버튼 #52

---
+++ src/pages/channel/config/index.ts
@@ -0,0 +1,5 @@
+export const CHANNEL_ACTION_KEY = {
+  create: 'channel-create',
+  update: 'channel-update',
+  delete: 'channel-delete',
+};
---
+++ src/pages/channel/ui/styles.ts
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants';
+
+export const listItem = tv({
+  base: 'border border-default-100 hover:shadow', // TODO: add height
+});
