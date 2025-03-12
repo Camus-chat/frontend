e0f4970dfdf7a3e0b57b23e653c18bc94f2ed49c
nijesmik <bkybb@duck.com>
2025-03-12 12:51:09 +0900

    :truck: create button > channel add button

---
+++ src/pages/channel/ui/channel-add-button.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Card, CardBody } from '@heroui/card';
+
+import { PlusIcon } from '@/shared/ui';
+import { useServicePopup } from '@/widgets/service-content';
+
+import { CHANNEL_ACTION_KEY } from '../config';
+import { listItem } from './styles';
+
+const ChannelAddButton = () => {
+  const open = useServicePopup((state) => state.open);
+
+  return (
+    <Card
+      shadow='none'
+      className={listItem()}
+      isPressable
+      onPress={() => open(CHANNEL_ACTION_KEY.create)}
+    >
+      <CardBody className='items-center justify-center text-default-500 hover:text-default-800'>
+        <PlusIcon />
+        채널 추가
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ChannelAddButton;
--- src/pages/channel/ui/channel-list.tsx
+++ src/pages/channel/ui/channel-list.tsx
@@ -1,9 +1,9 @@
-import CreateButton from './create-button';
+import ChannelAddButton from './channel-add-button';
 
 const ChannelList = () => {
   return (
     <div className='grid grid-cols-2 gap-3'>
-      <CreateButton />
+      <ChannelAddButton />
     </div>
   );
 };
