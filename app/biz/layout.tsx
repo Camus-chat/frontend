6751911ccc09e9a1b7e3eba0a8d40ede9e32e0f4
nijesmik <bkybb@duck.com>
2025-03-20 02:56:38 +0900

    :bug: 빌드 충돌 수정 - simplify layout component by exporting default layout

--- app/biz/layout.tsx
+++ app/biz/layout.tsx
@@ -1,18 +1,1 @@
-import type { FC, ReactNode } from 'react';
-
-import Header from '@/widgets/header';
-
-interface Props {
-  children: ReactNode;
-}
-
-const Layout: FC<Props> = ({ children }) => {
-  return (
-    <>
-      <Header business />
-      <main>{children}</main>
-    </>
-  );
-};
-
-export default Layout;
+export { default } from '@/app/layouts/default';
