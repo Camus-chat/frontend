cb2489f66b30275f7fa511d8f74a00bd72913022
전민정 <minj_1203@naver.com>
2025-03-23 21:25:02 +0900

    :lipstick: 채팅 입장 알림 메시지 tailwind-css 적용

---
+++ src/widgets/chat/ui/chat-notice.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  message: Message;
+  targetNickname: string;
+}
+
+const ChatNotice = ({ message, targetNickname }: Props) => {
+  return (
+    <div className='mx-0 my-2 flex justify-center text-sm text-[#858899]'>{`${targetNickname}${message.content}`}</div>
+  );
+};
+
+export default ChatNotice;
