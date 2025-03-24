744c453d1ca6dbefb487da5d953d2abcd05d47c3
nijesmik <bkybb@duck.com>
2025-03-24 16:27:57 +0900

    :sparkles: EnterAsMember 컴포넌트 예외처리 추가 - 로그인 상태가 아니면 로그인 페이지로 이동 - 본인의 채널인 경우 disabled 처리

--- src/pages/guest/ui/enter-as-member.tsx
+++ src/pages/guest/ui/enter-as-member.tsx
@@ -1,19 +1,39 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import { useMemberStore } from '@/entities/member';
+import { ROUTE } from '@/shared/config';
 import { Button } from '@/shared/ui';
 
 import { requestEnterChatting } from '../api/enter';
 
 const EnterAsMember: FC<{
   link: string;
-}> = ({ link }) => {
+  uuid: Channel.Info['uuid'];
+}> = ({ link, uuid }) => {
+  const member = useMemberStore((state) => state.member);
+  const isLoggedIn = !!member;
+  const isDisabled = isLoggedIn && member.uuid === uuid;
+  const router = useRouter();
+
+  const onClick = () => {
+    if (isLoggedIn) {
+      requestEnterChatting(link);
+    } else {
+      router.push(ROUTE.login);
+    }
+  };
+
   return (
     <Button
       color='primary'
       size='lg'
       className='w-full'
-      onClick={() => requestEnterChatting(link)}
+      onClick={onClick}
+      isDisabled={isDisabled}
     >
+      {!isDisabled && '로그인 후 '}
       참여하기
     </Button>
   );
