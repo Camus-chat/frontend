be1b7d655e71750c34e09904355ed22bec9e9db9
nijesmik <bkybb@duck.com>
2025-04-02 01:19:33 +0900

    :recycle: refactor message type

---
+++ src/entities/message.d.ts
@@ -0,0 +1,23 @@
+type MessageType = 'NoticeMessage' | 'CommonMessage' | 'FilteredMessage';
+
+namespace Message {
+  interface Base {
+    type: MessageType;
+    content: string;
+    messageId: number | string;
+    createdDate: string;
+  }
+
+  interface Notice extends Base {
+    type: 'NoticeMessage';
+    targetId: string;
+    noticeType: string;
+  }
+
+  interface Common extends Base {
+    senderId: string;
+    filteredLevel: number;
+  }
+}
+
+type Message = Message.Notice | Message.Common;
