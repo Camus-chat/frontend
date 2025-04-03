6929af331be8000c267f0cb7212a8e4ba942aa8b
nijesmik <bkybb@duck.com>
2025-04-03 23:23:58 +0900

    :recycle: refactor EnterAsGuest component and extract footer into separate file

--- src/pages/guest/ui/enter-as-guest.tsx
+++ src/pages/guest/ui/enter-as-guest.tsx
@@ -4,19 +4,13 @@ import {
   Modal,
   ModalBody,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   useDisclosure,
 } from '@heroui/modal';
-import { useRouter } from 'next/navigation';
 
-import { signIn } from '@/features/login';
-import { ROUTE } from '@/shared/config';
-import { useTokenStore } from '@/shared/store';
 import { Button } from '@/shared/ui';
-import { useChattingRoomStore } from '@/widgets/chatting';
 
-import { guestSignUp, requestEnterChatting } from '../api';
+import EnterAsGuestFooter from './enter-as-guest-footer';
 
 interface Props {
   link: string;
@@ -24,38 +18,18 @@ interface Props {
 
 const EnterAsGuest = ({ link }: Props) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const router = useRouter();
-
-  const handleEnterRoom = async () => {
-    const userData = await guestSignUp();
-    await signIn(userData)
-      .then((res) => {
-        const { setToken } = useTokenStore.getState();
-        setToken(res);
-      })
-      .catch((err) => {
-        if (process.env.NODE_ENV === 'development') {
-          console.log(err);
-        }
-        alert('게스트 로그인에 실패했습니다.');
-      });
-
-    const roomId = await requestEnterChatting(link);
-    if (!roomId) {
-      alert('채팅방에 입장에 실패했습니다.');
-      return;
-    }
-
-    useChattingRoomStore.getState().setRoomId(roomId);
-    router.push(`${ROUTE.guest}/${link}/${roomId}`);
-  };
 
   return (
     <>
       <Button variant='light' className='w-full' onClick={onOpen}>
         게스트로 참여하기
       </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} hideCloseButton>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        hideCloseButton
+        isDismissable={false}
+      >
         <ModalContent>
           {(onClose) => (
             <>
@@ -66,14 +40,7 @@ const EnterAsGuest = ({ link }: Props) => {
                   정말 게스트로 참여하시겠습니까?
                 </p>
               </ModalBody>
-              <ModalFooter>
-                <Button color='danger' variant='light' onClick={onClose}>
-                  닫기
-                </Button>
-                <Button color='primary' onClick={handleEnterRoom}>
-                  참여하기
-                </Button>
-              </ModalFooter>
+              <EnterAsGuestFooter onClose={onClose} link={link} />
             </>
           )}
         </ModalContent>
