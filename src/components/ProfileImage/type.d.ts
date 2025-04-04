d45131ce39c9f9f625989d26ce3a6c49968b59b5
nijesmik <bkybb@duck.com>
2025-04-04 18:12:37 +0900

    :fire: 레거시 코드 삭제

--- src/components/ProfileImage/type.d.ts
+++
@@ -1,8 +0,0 @@
-import {
-  COLORS,
-  PROFILE_IMAGE_SIZE,
-} from '@/components/ProfileImage/constants';
-
-type ProfileImageSize = keyof typeof PROFILE_IMAGE_SIZE;
-
-type ProfileColor = (typeof COLORS)[number];
