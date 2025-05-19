9bcf63cab287b3c5b6d0ebda0385b5c5482da667
nijesmik <bkybb@duck.com>
2025-05-20 02:47:34 +0900

    :sparkles: add `isFixed` prop to Header component for fixed positioning

--- src/app/layouts/default.tsx
+++ src/app/layouts/default.tsx
@@ -11,7 +11,7 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => {
   return (
     <AuthProvider>
-      <Header />
+      <Header isFixed />
       <main>{children}</main>
     </AuthProvider>
   );
