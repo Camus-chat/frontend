e4b2669cd87ced01ef035cff66086c1c99b354e7
nijesmik <bkybb@duck.com>
2025-05-26 13:53:00 +0900

    :lipstick: Mockup 컴포넌트 색상 변경

--- src/shared/ui/component/mockup.tsx
+++ src/shared/ui/component/mockup.tsx
@@ -8,17 +8,17 @@ const createStyle = tv({
   slots: {
     base: [
       'aspect-[9/18] min-h-mockup w-full max-w-[410px]',
-      'relative rounded-[2.5rem] border-[14px] border-gray-800 bg-gray-800 dark:border-gray-800',
+      'relative rounded-[2.5rem] border-[14px] border-default-800 bg-default-800 dark:border-default-800',
       'max-md:mx-3',
     ],
     buttonLeft: [
       'top-[72px] h-[46px]', // adjust this value to change the position and height of the left button
-      'absolute -start-[17px] w-[3px] rounded-s-lg bg-gray-800 dark:bg-gray-800',
+      'absolute -start-[17px] w-[3px] rounded-s-lg bg-default-800 dark:bg-default-800',
     ],
     buttonRight:
-      'absolute -end-[17px] top-[142px] h-[64px] w-[3px] rounded-e-lg bg-gray-800 dark:bg-gray-800',
+      'absolute -end-[17px] top-[142px] h-[64px] w-[3px] rounded-e-lg bg-default-800 dark:bg-default-800',
     wrapper:
-      'size-full overflow-hidden rounded-[2rem] bg-white dark:bg-gray-800',
+      'size-full overflow-hidden rounded-[2rem] bg-white dark:bg-default-800',
   },
 });
 
