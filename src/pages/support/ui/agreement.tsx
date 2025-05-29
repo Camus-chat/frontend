fe1c219c14f11c0d0ef755e1944ce4650a1db4c7
전민정 <minj_1203@naver.com>
2025-05-29 22:48:06 +0900

    :lipstick: support 페이지 checkbox-agreement UI 제작

---
+++ src/pages/support/ui/agreement.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Checkbox } from '@heroui/react';
+import Link from 'next/link';
+
+import { useAssistDataStore } from '@/pages/support/store/assist-data';
+
+const Agreement = () => {
+  const isAgreed = useAssistDataStore((state) => state.isAgreed);
+  const setIsAgreed = useAssistDataStore((state) => state.setIsAgreed);
+
+  return (
+    <Checkbox
+      className='mt-2 max-w-full items-start pr-0'
+      isSelected={isAgreed}
+      onValueChange={setIsAgreed}
+    >
+      <p className='text-sm font-light text-gray-400'>
+        {"I agree to CAMUS's "}
+        <Link href='/terms-of-service' className='font-normal text-blue-600'>
+          Terms of Service
+        </Link>
+        {' and '}
+        <Link href='/privacy-notice' className='font-normal text-blue-600'>
+          Privacy Policy
+        </Link>
+        {
+          ' which includes my consent to receive marketing information from CAMUS. I can unsubscribe from marketing communications at any time.'
+        }
+      </p>
+    </Checkbox>
+  );
+};
+
+export default Agreement;
