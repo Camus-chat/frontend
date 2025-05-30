ebf9f216b84912cb0e4cf16aae3e61474087f181
nijesmik <bkybb@duck.com>
2025-05-30 12:34:28 +0900

    :lipstick: Hero 컴포넌트 스타일 개선 및 props 변경

--- src/pages/landing/ui/page.tsx
+++ src/pages/landing/ui/page.tsx
@@ -4,7 +4,7 @@ import TutorialChattingDescription from './tutorial-chatting/description';
 
 const LandingPage = () => {
   return (
-    <Hero classNames={{ base: 'pt-14 md:pt-22' }}>
+    <Hero className='bg-sky-50/40' isFirst>
       <TutorialChattingDescription />
       <TutorialChatting />
     </Hero>
