ca5cea17bccbf6ec727b78eaec1ec2153a8b7ca9
nijesmik <bkybb@duck.com>
2025-06-05 02:39:26 +0900

    :recycle: 자동 스크롤 기능을 Messages 컴포넌트로 이동

--- src/features/messages/ui/messages.tsx
+++ src/features/messages/ui/messages.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import MessageBubble from './message';
 
 interface Props {
@@ -6,22 +8,34 @@ interface Props {
 }
 
 const Messages = ({ messages, filterLevel }: Props) => {
-  return messages.map((message, index) => {
-    const key =
-      message.type === 'NoticeMessage'
-        ? message.messageId
-        : `${message.messageId}-${message.filteredLevel}`;
+  const ref = useRef<HTMLDivElement>(null);
 
-    return (
-      <MessageBubble
-        key={key}
-        message={message}
-        prevMessage={messages[index - 1]}
-        nextMessage={messages[index + 1]}
-        chattingFilterLevel={filterLevel}
-      />
-    );
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
   });
+
+  return (
+    <div ref={ref} className='overflow-y-auto pb-2 scrollbar-hide'>
+      {messages.map((message, index) => {
+        const key =
+          message.type === 'NoticeMessage'
+            ? message.messageId
+            : `${message.messageId}-${message.filteredLevel}`;
+
+        return (
+          <MessageBubble
+            key={key}
+            message={message}
+            prevMessage={messages[index - 1]}
+            nextMessage={messages[index + 1]}
+            chattingFilterLevel={filterLevel}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Messages;
--- src/pages/landing/ui/tutorial-chatting/chatting-body.tsx
+++ src/pages/landing/ui/tutorial-chatting/chatting-body.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { CardBody, CardFooter } from '@heroui/react';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { Messages } from '@/features/messages';
 import { TUTORIAL_CHAT_MEMBER_ID } from '@/shared/config';
@@ -12,7 +12,6 @@ import { TUTORIAL_CHAT_FILTER_LEVEL } from '../../config';
 import { createMessage } from '../../lib/tutorial-chatting';
 
 const TutorialChattingBody = () => {
-  const ref = useRef<HTMLDivElement>(null);
   const [messages, setMessages] = useState<Message[]>([
     createMessage('안녕하세요', TUTORIAL_CHAT_MEMBER_ID.ai),
     createMessage('무엇을 도와드릴까요?', TUTORIAL_CHAT_MEMBER_ID.ai),
@@ -28,21 +27,13 @@ const TutorialChattingBody = () => {
     });
   };
 
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.scrollTop = ref.current.scrollHeight;
-    }
-  }, [messages]);
-
   return (
     <>
-      <CardBody className='overflow-y-hidden p-0'>
-        <div ref={ref} className='overflow-y-auto px-1'>
-          <Messages
-            messages={messages}
-            filterLevel={TUTORIAL_CHAT_FILTER_LEVEL}
-          />
-        </div>
+      <CardBody className='px-1 py-0'>
+        <Messages
+          messages={messages}
+          filterLevel={TUTORIAL_CHAT_FILTER_LEVEL}
+        />
       </CardBody>
       <CardFooter className='px-1 pb-2 pt-3'>
         <ChattingInput onSendMessage={handleSendMessage} />
