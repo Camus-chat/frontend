fcaa1dfeab5a8da80fc510f918a9acc086c31111
nijesmik <bkybb@duck.com>
2025-06-06 06:58:47 +0900

    :sparkles: landing page에 header 컴포넌트 및 anchor point 컴포넌트 추가

--- src/pages/landing/ui/page.tsx
+++ src/pages/landing/ui/page.tsx
@@ -1,5 +1,7 @@
 import { Hero } from '@/shared/ui';
+import Header from '@/widgets/header';
 
+import AnchorPoint from './anchor-point';
 import SupportDescription from './support/description';
 import SupportForm from './support/form';
 import TutorialChatting from './tutorial-chatting/chatting';
@@ -8,14 +10,21 @@ import TutorialChattingDescription from './tutorial-chatting/description';
 const LandingPage = () => {
   return (
     <>
-      <Hero className='bg-sky-50/40' isFirst>
-        <TutorialChattingDescription />
-        <TutorialChatting />
-      </Hero>
-      <Hero className='bg-[url(/images/background-support.svg)] bg-cover bg-center bg-no-repeat'>
-        <SupportDescription />
-        <SupportForm />
-      </Hero>
+      <Header isFixed>
+        <nav id='nav-menu' className='flex gap-8' />
+      </Header>
+      <main>
+        <AnchorPoint label='소개' anchorPortalId='nav-menu' />
+        <Hero className='bg-sky-50/40' isFirst>
+          <TutorialChattingDescription />
+          <TutorialChatting />
+        </Hero>
+        <AnchorPoint label='문의하기' anchorPortalId='nav-menu' />
+        <Hero className='bg-[url(/images/background-support.svg)] bg-cover bg-center bg-no-repeat'>
+          <SupportDescription />
+          <SupportForm />
+        </Hero>
+      </main>
     </>
   );
 };
