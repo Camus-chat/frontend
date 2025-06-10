531c7e77241990626972d60bcd41248935c2638b
nijesmik <bkybb@duck.com>
2025-06-10 16:48:56 +0900

    :lipstick: footer 컴포넌트 ui 수정 #132

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -18,33 +18,35 @@ const Footer = () => {
   const styles = createStyle();
 
   return (
-    <footer className='wrapper hero w-full bg-foreground-900'>
-      <div className='flex grid-cols-6 flex-col gap-5 px-2 md:grid'>
-        <div
-          className={styles.column({
-            class: 'col-start-1 col-end-3',
-          })}
-        >
-          <Logo className='text-foreground-200' business={false} />
+    <footer className='hero w-full bg-foreground-900'>
+      <div className='wrapper'>
+        <div className='flex grid-cols-6 flex-col gap-5 px-2 md:grid'>
+          <div
+            className={styles.column({
+              class: 'col-start-1 col-end-3',
+            })}
+          >
+            <Logo className='text-foreground-200' business={false} />
+          </div>
+          <Divider className={styles.divider()} />
+          <div className={styles.column({ class: 'col-start-5' })}>
+            <span className={styles.title()}>서비스</span>
+            <Link className={styles.text()} href={ROUTE.chat}>
+              채팅
+            </Link>
+            <Link className={styles.text()} href={ROUTE.channel}>
+              채널
+            </Link>
+          </div>
+          <Divider className={styles.divider()} />
+          <div className={styles.column({ class: 'col-start-6' })}>
+            <span className={styles.title()}>계정</span>
+            <Link className={styles.text()} href={ROUTE.login}>
+              로그인
+            </Link>
+          </div>
+          <Divider className={styles.divider()} />
         </div>
-        <Divider className={styles.divider()} />
-        <div className={styles.column({ class: 'col-start-5' })}>
-          <span className={styles.title()}>서비스</span>
-          <Link className={styles.text()} href={ROUTE.chat}>
-            채팅
-          </Link>
-          <Link className={styles.text()} href={ROUTE.channel}>
-            채널
-          </Link>
-        </div>
-        <Divider className={styles.divider()} />
-        <div className={styles.column({ class: 'col-start-6' })}>
-          <span className={styles.title()}>계정</span>
-          <Link className={styles.text()} href={ROUTE.login}>
-            로그인
-          </Link>
-        </div>
-        <Divider className={styles.divider()} />
       </div>
     </footer>
   );
