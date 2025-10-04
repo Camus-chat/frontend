e00779549b5aec604cdbbd0a65509aada4b00c6f
nijesmik <bkybb@duck.com>
2025-10-05 07:11:02 +0900

    :bug: `ServiceLayout`에 `h-0` 스타일 추가 - 스타일 관리 방식을 Tailwind Variants로 변경

--- src/app/layouts/service.tsx
+++ src/app/layouts/service.tsx
@@ -1,4 +1,5 @@
 import type { FC, ReactNode } from 'react';
+import { tv } from 'tailwind-variants';
 
 import Menu from '@/features/service-menu';
 import Header from '@/widgets/header';
@@ -9,11 +10,19 @@ interface Props {
   children: ReactNode;
 }
 
+const style = tv({
+  base: [
+    'relative overflow-y-hidden',
+    'h-0 grow', // mobile
+    'md:grid md:grid-cols-[auto,1fr] md:bg-default-100', // desktop
+  ],
+});
+
 const ServiceLayout: FC<Props> = ({ children }) => {
   return (
     <AuthProvider requireAuth={process.env.NODE_ENV !== 'development'}>
       <Header className='max-w-none' />
-      <main className='relative grow overflow-y-hidden md:grid md:grid-cols-[auto,1fr] md:bg-default-100'>
+      <main className={style()}>
         <Menu />
         {children}
       </main>
