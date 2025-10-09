e8c86ebd82cca9d40404f56c3723fdee674a77c9
nijesmik <bkybb@duck.com>
2025-10-09 17:45:36 +0900

    :sparkles: 랜딩 페이지 form 스토리 추가

---
+++ src/pages/landing/ui/support/index.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+
+import { Hero } from '@/shared/ui';
+
+import SupportDescription from './description';
+import SupportForm from './form';
+
+const Support = () => {
+  return (
+    <Hero className='bg-[url(/images/background-support.svg)] bg-cover bg-center bg-no-repeat'>
+      <SupportDescription />
+      <SupportForm />
+    </Hero>
+  );
+};
+
+const meta = {
+  title: 'Page/Landing/SupportForm',
+  component: Support,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Support>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
