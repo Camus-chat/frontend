ab1c22a07736773911694c9f13a7dea63e237c78
nijesmik <bkybb@duck.com>
2025-10-12 08:16:53 +0900

    :sparkles: page not found 스토리 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,25 @@
+import { HeroUIProvider } from '@heroui/react';
 import type { Preview } from '@storybook/nextjs-vite';
 
 import '@/app/styles';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <HeroUIProvider className='flex min-h-screen w-screen flex-col'>
+        <Story />
+      </HeroUIProvider>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    nextjs: {
+      appDirectory: true,
     },
   },
 };
